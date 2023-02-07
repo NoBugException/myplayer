acd18709c8b0e8143bfff1bb51452b04d0ac7bd3
zhouyunchong <Yunchong.Zhou@luxshare-ict.com>
2023-02-07 16:53:08 +0800

    播放器部分代码

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,14 +1,24 @@
 #include <jni.h>
 #include <string>
+#include "DNFFmpeg.h"
 
-extern "C"{
-#include <libavcodec/avcodec.h>
+DNFFmpeg *ffmpeg = 0;
+
+JavaVM *javaVm = 0;
+
+int JNI_OnLoad(JavaVM *vm, void *r) {
+    javaVm = vm;
+    return JNI_VERSION_1_6;
 }
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_android_myplayer_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(av_version_info());
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_android_myplayer_MyPlayer_native_1prepare(JNIEnv *env, jobject instance, jstring data_source) {
+    const char *dataSource = env->GetStringUTFChars(data_source, 0);
+    // 创建播放器
+    JavaCallHelper *helper = new JavaCallHelper(javaVm, env, instance);
+    ffmpeg = new DNFFmpeg(helper, dataSource);
+    ffmpeg->prepare();
+    env->ReleaseStringUTFChars(data_source, dataSource);
 }
